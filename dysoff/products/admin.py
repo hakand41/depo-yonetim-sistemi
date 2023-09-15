1c2f7c93cd6592e459ab7f941410404a60aae2d7
Hakan Değer <98481026+hakand41@users.noreply.github.com>
2023-09-15 15:23:18 +0300

    admin-py
    admin paneli ve loglar

--- dysoff/products/admin.py
+++ dysoff/products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Resim, Urun, Fatura
+import logging
 
 # Register your models here.
+logger = logging.getLogger(__name__)
+
+class MyModelAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        # Model kaydedilmeden önce log oluşturun
+        logger.info(f'{obj} kaydedildi.')
+
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Resim, MyModelAdmin)
+admin.site.register(Urun, MyModelAdmin)
+admin.site.register(Fatura, MyModelAdmin)
